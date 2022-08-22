f85eade56d2108c34cfd1646cbb83259fd7dc8a0
WuBoytH <alexwuief@gmail.com>
2022-08-22 08:51:18 -0400

    One more reorg

---
+++ scripts/make_dist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.9
+import shutil, os, sys, pathlib
+
+# check if romfs exists
+if not os.path.exists("../romfs"):
+  print("ERROR: no romfs dir!")
+  exit("romfs dir was missing!")
+
+# if distribution folder exists, delete it
+if "build" in os.listdir('..'):
+  shutil.rmtree('../build')
+os.makedirs('../build/ultimate/')
+shutil.copytree("../romfs", "../build/ultimate/mods")
+
+
+# zip each folder in the staging dir
+shutil.make_archive("build", 'zip', '../build')
+shutil.move("build.zip", "../build.zip")
